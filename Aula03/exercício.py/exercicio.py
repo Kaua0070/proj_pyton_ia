f792d8601e97643e1ea4c6b6107243ecdc4b3b58
kaua oliveira <kauasantto@gmail.com>
2024-08-28 20:19:59 -0300

    exercicio

---
+++ Aula03/exercício.py/exercicio.py
@@ -0,0 +1,17 @@
+comidas = [
+    "melancia",
+    "maça",
+    "abacate",
+    "abacaxi",
+    "açaí",
+    "acerola",
+    "amora",
+    "araticum",
+    "banana",
+    "cacau",
+]
+comidas.sort()
+y = 1
+for x in comidas:
+    print(y,"-",x)
+    y = y + 1
---
+++ Aula03/para2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "kaua",
+    "ana",
+    "malik",
+    "kawan",
+    "giovanna",
+    "isadora",
+    "machista",
+    "gustavo",
+    "richard",
+    "diogo"
+]
+alunos.sort()
+alunos.append("fausto silva")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
